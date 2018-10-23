4c7e2b835e71e1305b8df00ef5d5709ef1b0cdf9
Lucas Takeshi <lucastakeshipalma@gmail.com>
2018-10-23 18:38:25 -0300

    Criando um filtro por tipos de dinamicas

---
+++ wwwroot/js/filtro.js
@@ -0,0 +1,14 @@
+const listaDinamicas = document.querySelectorAll(".dinamica");
+document.querySelector(".filtro-botao").addEventListener("click", () =>{
+    listaDinamicas.forEach(dinamica => {
+        dinamica.style.display = "block";
+    })
+    document.querySelectorAll(".filtro-tipo:checked").forEach(campoTipo => {
+        const tipo = campoTipo.value;
+        listaDinamicas.forEach(dinamica => {
+            if(dinamica.dataset.tipos.toString().indexOf(tipo) < 0) {
+                dinamica.style.display = "none";
+            }
+        })
+    })
+});
